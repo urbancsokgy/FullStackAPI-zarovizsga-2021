dc107d0d4bb3c896e30ad99d107cdf7624e11a11
urbancsokgy <urbancsokgy@gmail.com>
2021-08-09 14:16:40 +0200

    Backend service model

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,42 @@
 // 1. Feladat: pótold a service modellt.
+/*
+
+export class Service {
+  _id: string = '';
+  name: string = '';
+  since: number = 0;
+  street?: string = '';
+  city?: string = '';
+  country?: string = '';
+  active?: boolean = true;
+}
+*/
+const mongoose = require('mongoose');
+
+const ServiceSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    since: {
+        type: Number,
+        required: true
+    },
+    street: {
+        type: String,        
+    },
+    city: {
+        type: String,        
+    },
+    country: {
+        type: String,        
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timeStamps: true
+});
+
+module.exports = mongoose.model('Service', ServiceSchema);
